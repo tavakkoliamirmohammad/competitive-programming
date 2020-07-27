28c10991b6e665cffff1bebc62ce49581b7d96db
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2020-07-27 14:35:05 +0430

    add uva 12279 wrong answer

---
+++ introduction/getting_started_easy_problems/super_easy/uva_12279.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int k;
+    int i = 0;
+    cin >> k;
+    while (k != 0) {
+        string line;
+        cin.ignore();
+        getline(cin, line);
+        stringstream in(line);
+        int temp;
+        int book_balance = 0;
+        int expected_balance = 0;
+        while (in >> temp) {
+            if (temp == 0)
+                expected_balance++;
+            else
+                book_balance++;
+        }
+        cout << "Case " << i + 1 << ": " << book_balance - expected_balance << endl;
+        cin >> k;
+        ++i;
+    }
+    return 0;
+}
