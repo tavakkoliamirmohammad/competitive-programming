e25541fa18d7d32603da6c814e5f42de8d662067
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2020-07-27 17:30:25 +0430

    add uva 272

--- introduction/getting_started_easy_problems/super_easy/uva_272.cpp
+++ introduction/getting_started_easy_problems/super_easy/uva_272.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 
-int main(){
-    cout << "hello world" << endl;
+int main() {
+    string line;
+    bool first = false;
+    while (getline(cin, line)) {
+        int pos;
+        while ((pos = line.find('\"')) != string::npos) {
+            line.replace(pos, 1, !first ? "``" : "''");
+            first = !first;
+        }
+        cout << line << endl;
+    }
     return 0;
 }
