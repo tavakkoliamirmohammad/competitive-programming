3009db05a45d38da7b495691b9ecd4e2bba40479
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2020-07-27 17:29:11 +0430

    add uva 12289

---
+++ introduction/getting_started_easy_problems/super_easy/uva_12289.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string one = "one";
+    string three = "three";
+    string two = "two";
+    for (int i = 0; i < n; ++i) {
+        string word;
+        cin >> word;
+    }
+    return 0;
+}
