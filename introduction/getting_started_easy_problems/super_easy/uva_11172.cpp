c14f1122be32d3b035e42d5dca70b58091d07c2f
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2020-07-27 13:07:41 +0430

    add uva 11172

---
+++ introduction/getting_started_easy_problems/super_easy/uva_11172.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int a, b;
+        cin >> a >> b;
+        if (a > b)
+            cout << ">" << endl;
+        else if (a < b)
+            cout << "<" << endl;
+        else
+            cout << "=" << endl;
+    }
+    return 0;
+}
