97dc4232c101f929dee6f4f7bec382b8dd240c1e
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2020-07-28 18:20:01 +0430

    fix bug in uva 12468

--- introduction/getting_started_easy_problems/easy/uva_12468.cpp
+++ introduction/getting_started_easy_problems/easy/uva_12468.cpp
@@ -7,7 +7,8 @@ int main() {
     int start, end;
     cin >> start >> end;
     while (start != -1 && end != -1) {
-        cout << min(abs(end - start), abs(start - end + 99 + 1)) << endl;
+        int distance = abs(start - end);
+        cout << min(100 - distance, distance) << endl;
         cin >> start >> end;
     }
     return 0;
