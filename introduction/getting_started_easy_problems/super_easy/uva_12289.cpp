8dd5e11eb20ea3d5aa64cf96fdc4588781a3f32e
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2020-08-01 13:37:22 +0430

    add uva 12289

--- introduction/getting_started_easy_problems/super_easy/uva_12289.cpp
+++ introduction/getting_started_easy_problems/super_easy/uva_12289.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+
+int matching_characters(string s1, string s2) {
+    sort(begin(s1), end(s1));
+    sort(begin(s2), end(s2));
+    string intersection;
+    set_intersection(begin(s1), end(s1), begin(s2), end(s2),
+                     back_inserter(intersection));
+    return intersection.size();
+}
+
 
 int main() {
     int n;
     cin >> n;
-    string one = "one";
-    string three = "three";
-    string two = "two";
+    vector<string> words = {"one", "two", "three"};
     for (int i = 0; i < n; ++i) {
-        string word;
-        cin >> word;
+        vector<int> diffs;
+        string w;
+        cin >> w;
+        for (const string& word : words) {
+            diffs.push_back(w.size() - matching_characters(w, word));
+        }
+        cout << min_element(diffs.begin(), diffs.end()) - diffs.begin() + 1<< endl;
+
     }
     return 0;
 }
